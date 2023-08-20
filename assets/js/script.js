5ee570819dee89bb0540fdedeae632509eda19c5
matus42 <134312272+matus42@users.noreply.github.com>
2023-08-20 19:45:08 +0100

    Add function to change color when click on answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,6 +115,10 @@ function showQuestion(){
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButtons.appendChild(button);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
   });
 }
 //removes all the previous answers
@@ -122,6 +126,16 @@ function resetState(){
   nextButton.style.display = "none";
   while(answerButtons.firstChild){
     answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e){
+  let selectedBtn = e.target;
+  let isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+  }else{
+    selectedBtn.classList.add("incorrect");
   }
 }
 
